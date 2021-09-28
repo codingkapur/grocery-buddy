4918a6bc54c05689a4b5e649c4265abdde6fec5a
Raghav <codingkapur@gmail.com>
2021-09-28 12:26:56 +0530

    initial commit with basic UI ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const onSubmit= (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__container">
+      <h1 className="title">Grocery Buddy</h1>
+      <form className="form" onSubmit={onSubmit}>
+        <input type="text" placeholder="Add an item" className="form__input"/>
+        <button type="submit" className="form__submit">Submit</button>
+      </form>
     </div>
   );
 }
